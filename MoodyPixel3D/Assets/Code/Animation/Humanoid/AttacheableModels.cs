2a76ab9bca015d2f7f25201d27cdca274605ca67
João Brant <brant.reis@gmail.com>
2020-10-05 19:19:35 -0300

    FMOD plus wearables start

---
+++ MoodyPixel3D/Assets/Code/Animation/Humanoid/AttacheableModels.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttacheableModels : MonoBehaviour
+{
+    public Transform head, leftHand, rightHand, neck;
+}
