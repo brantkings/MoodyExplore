241c2a862f811e5f03a5f36719a29e3884e8be0b
lmattos <lrasomattos@gmail.com>
2020-10-09 02:15:38 -0300

    Sensor and Focus system ready.

---
+++ MoodyPixel3D/Assets/Code/FocusSystem/FocusableUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FocusableUI : MonoBehaviour
+{
+    Focusable _focusable;
+
+    public Image iconImage;
+    public PointsAsObjectsUI focusPointsUI;
+
+    public void SetFocusable(Focusable focusable)
+    {
+        if (_focusable == focusable)
+            return;
+
+        if(_focusable != null)
+        {
+            _focusable.OnFocusChanged -= Focusable_OnFocusChanged;
+        }
+
+        _focusable = focusable;
+
+        if (_focusable == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        if (iconImage != null)
+            iconImage.sprite = _focusable.focusableIcon;
+
+        _focusable.OnFocusChanged += Focusable_OnFocusChanged;
+    }
+
+    private void Focusable_OnFocusChanged(int newFocus)
+    {
+        if (focusPointsUI == null)
+            return;
+
+        focusPointsUI.SetNPoints(newFocus);
+    }
+}
